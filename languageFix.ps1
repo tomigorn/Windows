b1ec48b54b89c4cffb2b1345506d36185023b4c5
tomigorn <49411412+tomigorn@users.noreply.github.com>
2022-01-14 23:11:48 +0100

    Add files via upload

---
+++ languageFix.ps1
@@ -0,0 +1,5 @@
+Set-WinCultureFromLanguageListOptOut -OptOut 1
+Set-WinUILanguageOverride -Language en-GB
+Set-WinDefaultInputMethodOverride -InputTip "0807:00000807"
+Set-Culture de-CH
+Write-Host "language script done.."
